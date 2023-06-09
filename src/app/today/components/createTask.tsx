e42da769993cef892b658cf7706699ad8b72564c
Antony Kamau <kariuantony@gmail.com>
2023-06-09 16:31:24 +0000

    Edits to task creation

---
+++ src/app/today/components/createTask.tsx
@@ -0,0 +1,99 @@
+"use client";
+import createTask from "@/lib/createTask";
+import React, { useReducer } from "react";
+
+export default function CreateTask() {
+  const initial = {
+    title: "",
+    description: "",
+    priority: "0",
+    completed: false,
+  };
+
+  const [state, dispatch] = useReducer(
+    (state: TaskObj, action: Partial<TaskObj>) => ({
+      ...state,
+      ...action,
+    }),
+    initial
+  );
+
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+
+    createTask(state)
+      .then((task) => {
+        console.log(task);
+        dispatch(initial);
+      })
+      .catch((error) => console.log(error));
+  };
+
+  return (
+    <div className="container">
+      <form
+        onSubmit={handleSubmit}
+        className="w-4xl border border-divider-100 rounded-xl"
+      >
+        <div className="flex flex-wrap px-sm pt-sm">
+          <div className="mb-sm w-full">
+            <textarea
+              onInput={(e) => {
+                e.currentTarget.style.height = "";
+                e.currentTarget.style.height =
+                  Math.min(e.currentTarget.scrollHeight, 150) + "px";
+              }}
+              className="w-full bg-primary-100 focus:outline-none text-sm resize-none font-semibold"
+              onChange={(e) => dispatch({ title: e.target.value })}
+              value={state.title}
+              placeholder="Task name"
+              rows={1}
+            ></textarea>
+            <textarea
+              onInput={(e) => {
+                e.currentTarget.style.height = "";
+                e.currentTarget.style.height =
+                  Math.min(e.currentTarget.scrollHeight, 150) + "px";
+              }}
+              className="w-full bg-primary-100 focus:outline-none text-xs resize-none"
+              onChange={(e) => dispatch({ description: e.target.value })}
+              value={state.description}
+              placeholder="Description"
+              rows={1}
+            ></textarea>
+          </div>
+          <div className="mb-sm w-full">
+            <select
+              className="bg-primary-100 border border-divider-100 rounded-md px-2 py-1 text-xs"
+              name="priority"
+              onChange={(e) => dispatch({ priority: e.target.value })}
+            >
+              <option>Select</option>
+              <option value="1">🏳</option>
+              <option value="2">Medium</option>
+              <option value="3">High</option>
+              <option value="4">High</option>
+            </select>
+          </div>
+          <div className="mb-sm w-full flex flex-1 justify-end">
+            <button
+              type="reset"
+              className="bg-divider-100 px-sm-2 rounded-md text-sm mr-2"
+            >
+              Cancel
+            </button>
+            <button
+              type="submit"
+              className="bg-disabled-100 border border-disabled-100 px-sm-2 py-1 rounded-md text-sm disabled:opacity-50 disabled:cursor-not-allowed"
+              disabled={state.title === "" ? true : false}
+            >
+              Add Task
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,63 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
       backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        sm: "640px",
+        md: "768px",
+        lg: "1024px",
+        xl: "1280px",
+        "2xl": "1360px",
+        "3xl": "1440px",
+      },
+      spacing: {
+        sm: "10px",
+        "sm-2": "12px",
+        md: "16px",
+        lg: "24px",
+        xl: "32px",
+        "2xl": "40px",
+        "3xl": "48px",
+        "4xl": "800px",
+      },
+      fontSize: {
+        xs: "13px",
+        sm: "14px",
+        tiny: ".875rem",
+        base: "1rem",
+        lg: "1.125rem",
+        xl: "1.25rem",
+        "2xl": "1.5rem",
+        "3xl": "1.875rem",
+        "4xl": "2.25rem",
+        "5xl": "3rem",
+        "6xl": "4rem",
+        "7xl": "5rem",
+      },
+      colors: {
+        primary: {
+          100: "#282828",
+          200: "#292929",
+        },
+        disabled: {
+          100: "#dc4c3e",
+        },
+        divider: {
+          100: "#3d3d3d",
+          200: "#999",
+        },
       },
     },
   },
   plugins: [],
-}
+};
